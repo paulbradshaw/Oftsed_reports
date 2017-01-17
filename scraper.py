f9f132984a13757e9ce8a66d5fd6dd26228de004
Paul Bradshaw <paulonhismobile@gmail.com>
2017-01-17 20:54:46 +0000

    add pdftoxml lines

--- scraper.py
+++ scraper.py
@@ -6,20 +6,15 @@ import lxml.etree
 import lxml.html
 
 # Read in a page
-html = scraperwiki.scrape("https://reports.ofsted.gov.uk/provider/files/2631211/urn/103980.pdf")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#html = scraperwiki.scrape("https://reports.ofsted.gov.uk/provider/files/2631211/urn/103980.pdf")
+url = "https://reports.ofsted.gov.uk/provider/files/2631211/urn/103980.pdf"
+pdfdata = urllib2.urlopen(url).read()
+print "The pdf file has %d bytes" % len(pdfdata)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+xmldata = scraperwiki.pdftoxml(pdfdata)
+print "After converting to xml it has %d bytes" % len(xmldata)
+print "The first 2000 characters are: ", xmldata[:2000]
+
+# turn html into an lxml object called 'root'
+#root = lxml.html.fromstring(html)
+#xmldata = scraperwiki.pdftoxml(pdfdata)
